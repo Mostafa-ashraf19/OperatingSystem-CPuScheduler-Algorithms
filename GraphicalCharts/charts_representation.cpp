92642352ac963c85368ede7600fee9f0df5d85ad
Mostafa Ashraf <mostaphaashraf1996@gmail.com>
2020-04-13 22:27:17 +0200

    Charts Representations

---
+++ GraphicalCharts/charts_representation.cpp
@@ -0,0 +1,62 @@
+#include "charts_representation.h"
+#include"ui_mainwindow.h"
+void Charts_Representation::ChartView_settings()
+{
+current=0;
+ chartview->setRenderHint(QPainter::Antialiasing);
+
+ axis->setTitleText("Hello thier");
+ chart->setAxisX(axis);
+
+}
+
+Charts_Representation::Charts_Representation()
+{
+    chart = new QChart();
+    chart->legend()->hide();
+    chartview = new QChartView(chart);
+    chartview->setRenderHint(QPainter::Antialiasing);
+    //SetChart_Title();
+}
+
+Charts_Representation::~Charts_Representation()
+{
+   // delete chart;
+    //delete chartview;
+}
+
+void Charts_Representation::addProcess(std::string Process_name,
+                                       qreal From_X_Position,
+                                        qreal To_X_Position,
+                                       qreal _Process_index)
+{
+    Series.push_back(new QLineSeries);
+    for(static unsigned int i= 0; i <Series.size();i++)
+    {
+    Series[i]->append(From_X_Position,_Process_index);
+    Series[i]->append(To_X_Position,_Process_index);
+    }
+   // current++;
+   // Chartsettings();
+   // Chartsettings();
+}
+
+void Charts_Representation::Chartsettings()
+{
+
+
+    for(unsigned int i=0;i< Series.size();i++)
+        chart->addSeries(Series[i]);
+    chart->createDefaultAxes();
+
+}
+
+QChartView *Charts_Representation::ViewChart()
+{
+    return  chartview;
+}
+
+void Charts_Representation::SetChart_Title()
+{
+    chart->setTitle("Process Drawing Simulation");
+}
---
+++ GraphicalCharts/charts_representation.h
@@ -0,0 +1,55 @@
+#ifndef CHARTS_REPRESENTATION_H
+#define CHARTS_REPRESENTATION_H
+
+#include"mainwindow.h"
+
+// Manages the applications main settings like
+// widget initialization
+#include <QApplication>
+// The main window to which you add toolbars,
+// menubars, widgets and status bar
+#include <QtWidgets/QMainWindow>
+// Widget used to display charts
+#include <QtCharts/QChartView>
+// Used to draw bars representing data provided
+// grouped into categories
+#include <QtCharts/QBarSeries>
+// Represents 1 set of bars in a bar chart
+#include <QtCharts/QBarSet>
+// Displays the color used to represent each
+// QBarSet
+#include <QtCharts/QLegend>
+// Used to create a line chart
+#include <QtCharts/QLineSeries>
+// Used to change names on axis
+#include <QtCharts/QCategoryAxis>
+#include<QtCharts/QAbstractBarSeries>
+#include<QtCharts/QAbstractAxis>
+// Define the scope for your variables and functions
+QT_CHARTS_USE_NAMESPACE
+
+#include<vector>
+
+class Charts_Representation
+{
+
+    std::vector<QLineSeries *>  Series;
+    QChart *chart;
+    int current;
+    QChartView* chartview;
+    void ChartView_settings();
+    QAbstractAxis* axis;
+public:
+    explicit Charts_Representation();
+    ~Charts_Representation();
+    void addProcess(std::string Process_name,
+                    qreal From_X_Position,
+                     qreal To_X_Position,
+                    qreal _Process_index);
+     void Chartsettings();
+     QChartView* ViewChart();
+    void SetChart_Title();
+
+};
+
+#endif // CHARTS_REPRESENTATION_H
