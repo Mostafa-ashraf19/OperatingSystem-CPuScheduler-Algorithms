d0daf8e5e2cd5021498bb781b65e83827f389372
Mostafa Ashraf <mostaphaashraf1996@gmail.com>
2020-04-15 10:22:23 +0200

    Final macros

--- Macros_Definitions.h
+++ Macros_Definitions.h
@@ -2,12 +2,12 @@
 #define MACROS_DEFINITIONS_H
 
 
-#define FCFS static_cast<int>(0)
-#define SJF_PRIMITVE static_cast<int>(1)
-#define SJF_NON_PRIMITVE static_cast<int>(2)
-#define PRIORTY_PRIMITVE static_cast<int>(3)
-#define PRIORTY_NON_PRIMITVE static_cast<int>(4)
-#define ROUND_ROBEB  static_cast<int>(5)
+#define FCFS_ static_cast<int>(0)
+#define SJF_PRIMITVE_ static_cast<int>(1)
+#define SJF_NON_PRIMITVE_ static_cast<int>(2)
+#define PRIORTY_PRIMITVE_ static_cast<int>(3)
+#define PRIORTY_NON_PRIMITVE_ static_cast<int>(4)
+#define ROUND_ROBEB_  static_cast<int>(5)
 
 
 #endif // MACROS_DEFINITIONS_H
